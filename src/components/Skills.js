523005bb04e696e26bc6098e6e4584496a24222f
Dhinesh <a.dhinesh1@gmail.com>
2022-02-01 22:40:22 +0530

    Adding progress bar for skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,5 +1,21 @@
+import { Line, Circle } from 'rc-progress';
+import { Fragment } from 'react';
+
 export const Skills = () => {
     return (
-        <div>Skills</div>
+        <div className='skills-table'>
+
+            <div className='skills-table-row'>
+                <div className='skills-table-col'>
+                    Javascript - 4 Years
+                    <Line percent="70" strokeWidth="1" strokeColor="green" trailWidth="0.5" trailColor="grey" />
+                </div>
+
+                <div className='skills-table-col'>
+                    Java - 4 Years
+                    <Line percent="70" strokeWidth="1" strokeColor="green" trailWidth="0.5" trailColor="grey" />
+                </div>
+            </div>
+        </div>
     )
 }
