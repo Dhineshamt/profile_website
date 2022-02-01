58f9e3fae3c0fa9be016725c7800229c9b28d334
Dhinesh <a.dhinesh1@gmail.com>
2022-02-01 11:48:40 +0530

    modularizing certification container code

--- src/components/Certification.js
+++ src/components/Certification.js
@@ -1,24 +1,23 @@
 import CertificationContainer from './CertificationContainer'
 
 export const Certification = () => {
+    const awsObject = {
+        title: 'AWS Certified Solutions Architect Associate',
+        year: 2021,
+        desc: "AWS",
+        imgName: 'AWS'
+    }
+
+    const javaObject = {
+        title: 'Oracle Certified Java Associate Developer',
+        year: 2020,
+        desc: "Java",
+        imgName: 'Java'
+    }
     return (
         <>
-            <div className='grid-container'>
-                <div className="cert-img"><img className="cert-image" src={require('../images/AWS.png')}></img></div>
-                <div className="cert-title">AWS Certified Solutions Associate</div>
-                <div className="cert-desc">Description</div>
-                <div className="cert-date">Certified Year: 2021</div>
-            </div>
-
-            <div className='grid-container'>
-                <div className="cert-img"><img className="cert-image" src={require('../images/Java.png')}></img></div>
-                <div className="cert-title">Oracle Certified Associate Developer</div>
-                <div className="cert-desc">Description</div>
-                <div className="cert-date">Certified Year: 2020</div>
-            </div>
-
-            <CertificationContainer title="AWS Certified Solutions Associate" year="2021" desc="AWS" image-name="AWS"/>
-
+            <CertificationContainer {...awsObject} />
+            <CertificationContainer {...javaObject}/>
         </>
     )
 }
--- src/components/CertificationContainer.js
+++ src/components/CertificationContainer.js
@@ -1,16 +1,36 @@
 import { Fragment } from "react/cjs/react.production.min";
 
 const CertificationContainer = (props) => {
-    return(
+    return (
         <Fragment>
-        <div className='grid-container'>
-        <div className="cert-img"><img className="cert-image" src={require('../images/AWS.png')}></img></div>
-        <div className="cert-title">{props.title}</div>
-        <div className="cert-desc">Description: {props.desc}</div>
-        <div className="cert-date">Certified Year: {props.year}</div>
-        </div>
+            <div className='grid-container'>
+                <Fragment>
+                    {
+                        (() => {
+                            return imgPicker(props.imgName);
+                        }
+                        )()
+                    }
+                </Fragment>
+                <div className="cert-title">{props.title}</div>
+                <div className="cert-desc">Description: {props.desc}</div>
+                <div className="cert-date">Certified Year: {props.year}</div>
+            </div>
         </Fragment>
     )
+}
+
+const imgPicker = (imgName) => {
+    if (imgName === 'AWS') {
+        return (
+            <div className="cert-img"><img className="cert-image" src={require('../images/AWS.png')}></img></div>
+        )
+    }
+    else if (imgName === "Java") {
+        return (
+            <div className="cert-img"><img className="cert-image" src={require('../images/Java.png')}></img></div>
+        )
+    }
 }
 
 export default CertificationContainer;
