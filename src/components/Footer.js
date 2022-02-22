6ea3e6e6aa6271ced35c472d10c49bb8a697d4cd
Dhinesh <a.dhinesh1@gmail.com>
2022-02-22 22:54:37 +0530

    removing unneeded component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,29 +6,30 @@ import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
 export const Footer = () => {
     return (
         <>
-                <div className="footer">
-                    <div>
-                        <span className="fa-icon">
-                            <FontAwesomeIcon icon={faMobileAlt} size="" />
-                        </span>
-                        <span className="m-left">9944991706</span>
-                    </div>
-                    <div>
-                        <span className="fa-icon">
-                            <FontAwesomeIcon icon={faAt} />
-                        </span>
-                        <span className="m-left">dhineshamt@gmail.com</span>
-                    </div>
-                    <div>
-                        <span className="fa-icon">
-                            <FontAwesomeIcon icon={faLinkedin} />
-                        </span>
-                        <span className="m-left">dhinesh@linkedin</span>
-                    </div>
-                    <div>
-                        <button className="download-button">Download Profile</button>
-                    </div>
+            <div className="footer">
+                <div>
+                    <span className="fa-icon">
+                        <FontAwesomeIcon icon={faMobileAlt} size="" />
+                    </span>
+                    <span className="m-left">9944991706</span>
                 </div>
-            </>
+                <div>
+                    <span className="fa-icon">
+                        <FontAwesomeIcon icon={faAt} />
+                    </span>
+                    <span className="m-left">dhineshamt@gmail.com</span>
+                </div>
+                <div>
+                    <span className="fa-icon">
+                        <FontAwesomeIcon icon={faLinkedin} />
+                    </span>
+                    <span className="m-left">dhinesh@linkedin</span>
+                </div>
+                <div>
+                    {/* <button className="download-button">Download Profile</button> */}
+                    <a className="download-button" href="https://bit.ly/resume-dhinesh" >Download Profile</a>
+                </div>
+            </div>
+        </>
     )
 }
